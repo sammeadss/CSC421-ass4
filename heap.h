9fd95dd9d4c8f3d94106e7da1c5a196961970923
Samuel <meadss0115@gmail.com>
2026-04-29 20:48:51 -0400

    docs: added comments to header file

--- heap.h
+++ heap.h
@@ -1,21 +1,23 @@
 #ifndef HEAP_H
 #define HEAP_H
 
-#define HEAP_SIZE 65536
-#define NUM_CLASSES 32
+#define HEAP_SIZE 65536 /* total pool size in words */
+#define NUM_CLASSES 32 /* number of size-class free lists */
 
+// Header stored at the start of every chunk in the pool
 typedef struct Chunk {
-    struct Chunk *prev;
-    struct Chunk *next;
-    int size;
+    struct Chunk *prev; /* word 0 */
+    struct Chunk *next; /* word 1 */
+    int size; /* word 2 */
 } Chunk;
 
+// One head pointer per size class
 extern Chunk *free_lists[NUM_CLASSES];
 
-void init(void);
-void *newvec(int n);
-void freevec(void *p);
+void init(void); /* initialize the heap */
+void *newvec(int n); /* allocate n words */
+void freevec(void *p); /* free a previously allocated block */
 
-int size_class(int n);
+int size_class(int n); /* map size in words to a free list index */
 
 #endif
